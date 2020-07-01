6efaacc039c938557fab7b639f1f3145f66dde7d
chiew12301 <64896661+chiew12301@users.noreply.github.com>
2020-07-01 18:40:26 +0800

    LogoSceneandMember's Folder

---
+++ TheLostChild/Assets/Script/LogoSceneScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoSceneScript : MonoBehaviour
+{
+    public float waitTime = 5f;
+    void Start()
+    {
+        StartCoroutine(DelaySwitch());
+    }
+
+    IEnumerator DelaySwitch()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
