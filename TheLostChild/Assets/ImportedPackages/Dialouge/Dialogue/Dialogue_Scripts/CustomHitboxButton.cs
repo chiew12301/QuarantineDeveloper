40eeb36f3a02b4e707690baab37c040c10aaa943
EleenCYW <65888313+EleenCYW@users.noreply.github.com>
2020-07-26 00:30:55 +0800

    changed prefab

--- TheLostChild/Assets/ImportedPackages/Dialouge/Dialogue/Dialogue_Scripts/CustomHitboxButton.cs
+++ TheLostChild/Assets/ImportedPackages/Dialouge/Dialogue/Dialogue_Scripts/CustomHitboxButton.cs
@@ -7,9 +7,6 @@ public class CustomHitboxButton : MonoBehaviour
 {
     void Start()
     {
-        if (this.GetComponent<Image>() != null)
-        {
             this.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.9f;
-        }
     }
 }
