8b50512630f6850e2bef5131d1915e6fd03b9974
chiew12301 <64896661+chiew12301@users.noreply.github.com>
2020-07-26 03:59:50 +0800

    Lastupdate28_7

---
+++ TheLostChild/Assets/Script/BedRoomCabinetP1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedRoomCabinetP1 : MonoBehaviour
+{
+    public GameObject mirror;
+    public GameObject MusicBox;
+    // Update is called once per frame
+    void Update()
+    {
+        checkGiveMusicBox();
+    }
+
+    void checkGiveMusicBox()
+    {
+        if(mirror.GetComponent<PickUp>().isRiddleDone == true)
+        {
+            MusicBox.GetComponent<MusicBoxSwitchSceneScript>().performPickup();
+        }
+    }
+
+}
